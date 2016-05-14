e09948d63ef105ebee8f57efd0c1cd4e2b53fb23
jgoley <jgoley@gmail.com>
2016-05-14 18:46:06 -0400

    Create admin package

---
+++ packages/admin/package.js
@@ -0,0 +1,34 @@
+Package.describe({
+  name: 'tap:admin',
+  version: '0.0.1'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+
+  api.use('ecmascript');
+  api.use('underscore');
+  api.use('coffeescript');
+  api.use('templating');
+  api.use('reactive-var');
+  api.use('reactive-dict');
+
+  api.use('tap:models');
+  api.use('tap:accounts');
+  api.use('tap:views');
+
+  api.use('rubaxa:sortable');
+  api.use('accounts-password');
+  api.use('useraccounts:core');
+  api.use('momentjs:moment');
+  api.use('lepozepo:s3');
+  api.use('amr:parsley.js');
+
+  api.addFiles('convert.coffee', ['client']);
+  api.addFiles('edit_tour.coffee', ['client', 'server']);
+  api.addFiles('edit_tours.coffee', ['client', 'server']);
+  api.addFiles('editMedia.coffee', ['client', 'server']);
+  api.addFiles('fileUpload.coffee', ['client', 'server']);
+  api.addFiles('upload.coffee', ['client', 'server']);
+  api.addFiles('uploadProgress.coffee', ['client']);
+});
--- packages/controllers/package.js
+++ packages/controllers/package.js
@@ -16,22 +16,13 @@ Package.onUse(function(api) {
   api.use('tap:models');
   api.use('tap:accounts');
   api.use('tap:views');
+  api.use('tap:admin');
 
-  api.use('rubaxa:sortable');
-  api.use('accounts-password');
-  api.use('useraccounts:core');
   api.use('momentjs:moment');
-  api.use('lepozepo:s3');
-  api.use('amr:parsley.js');
 
   api.addFiles('archivedTours.coffee', ['client', 'server']);
   api.addFiles('childStops.coffee', ['client', 'server']);
-  api.addFiles('convert.coffee', ['client']);
   api.addFiles('currentTours.coffee', ['client', 'server']);
-  api.addFiles('edit_tour.coffee', ['client', 'server']);
-  api.addFiles('edit_tours.coffee', ['client', 'server']);
-  api.addFiles('editMedia.coffee', ['client', 'server']);
-  api.addFiles('fileUpload.coffee', ['client', 'server']);
   api.addFiles('header.coffee', ['client']);
   api.addFiles('help.coffee', ['client']);
   api.addFiles('helpers.coffee', ['client']);
@@ -45,6 +36,4 @@ Package.onUse(function(api) {
   api.addFiles('thumbnail.coffee', ['client']);
   api.addFiles('tour.coffee', ['client', 'server']);
   api.addFiles('tourDetails.coffee', ['client', 'server']);
-  api.addFiles('upload.coffee', ['client', 'server']);
-  api.addFiles('uploadProgress.coffee', ['client']);
 });
