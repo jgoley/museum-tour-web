cd95a8835678ef06d1d76b50f1021b0d31f63a19
jgoley <jgoley@gmail.com>
2017-11-04 22:29:39 -0400

    Update iOS icon list and add icon image assets

--- mobile-config.js
+++ mobile-config.js
@@ -7,17 +7,31 @@ App.info({
   website    : 'http://jonathangoley.com'
 });
 
-App.icons({
-  'iphone_2x'      : 'public/ios_icons/icon-60@2x.png',
-  'iphone_3x'      : 'public/ios_icons/icon-60@3x.png',
-  'ipad'           : 'public/ios_icons/icon-76.png',
-  'ipad_2x'        : 'public/ios_icons/icon-76@2x.png',
-  'ipad_pro'       : 'public/ios_icons/icon-83.5@2x.png',
-  'ios_settings'   : 'public/ios_icons/icon-small.png',
-  'ios_settings_2x': 'public/ios_icons/icon-small@2x.png',
-  'ios_settings_3x': 'public/ios_icons/icon-small@3x.png',
-  'ios_spotlight'  : 'public/ios_icons/icon-40.png',
-});
+App.icons
+  'iphone_legacy'           : 'public/ios_icons/icon-57.png', // 57x57
+  'iphone_legacy_2x'        : 'public/ios_icons/icon-114.png', // 114x114
+  'iphone_2x'               : 'public/ios_icons/icon-60@2x.png', // 120x120
+  'iphone_3x'               : 'public/ios_icons/icon-60@3x.png', // 180x180
+  'ipad'                    : 'public/ios_icons/icon-76.png', // 76x76
+  'ipad_2x'                 : 'public/ios_icons/icon-76@2x.png', // 152x152
+  'ipad_pro'                : 'public/ios_icons/icon-83.5@2x.png', // 176x176
+  'ipad_app_legacy'         : 'public/ios_icons/icon-72.png', // 72x72
+  'ipad_app_legacy_2x'      : 'public/ios_icons/icon-144.png', // 144x144
+  'ios_settings'            : 'public/ios_icons/icon-small.png', // 29x29
+  'ios_settings_2x'         : 'public/ios_icons/icon-small@2x.png', // 59x59
+  'ios_settings_3x'         : 'public /ios_icons/icon-small@3x.png', // 87x87
+  'ios_notification'        : 'public/ios_icons/icon-20.png', // 20x20
+  'ios_notification_2x'     : 'public/ios_icons/icon-40.png', // 40x40
+  'ios_notification_3x'     : 'public/ios_icons/icon-60.png', // 60x60
+  'ios_spotlight'           : 'public/ios_icons/icon-40.png', // 40x40
+  'ios_spotlight_2x'        : 'public/ios_icons/icon-40.png', // 40x40
+  'ios_spotlight_3x'        : 'public/ios_icons/icon-60.png', // 60x60
+  'ipad_spotlight_legacy'   : 'public/ios_icons/icon-50.png' // 50x50
+  'ipad_spotlight_legacy_2x': 'public/ios_icons/icon-100.png' // 100x100
+  'iTunesArtwork'           : 'public/ios_icons/icon-40.png', // 40x40
+  'iTunesArtwork@2x'        : 'public/ios_icons/icon-40@2x.png', // 80x80
+  'app_store'               : 'public/ios_icons/app_store.png', // 1024x1024
+;
 
 App.launchScreens({
   'iphone_2x'         : 'public/ios_lauchScreens/iphone@2x.png',
